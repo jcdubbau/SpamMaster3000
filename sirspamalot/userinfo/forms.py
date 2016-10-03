28e15aa751237a23322c9cacfb51a04569a931df
jasoncwilley <jaycw0001@gmail.com>
2016-10-02 23:41:02 -0400

    template and view changes

--- sirspamalot/userinfo/forms.py
+++ sirspamalot/userinfo/forms.py
@@ -2,10 +2,9 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import SpamMessage, Profile
 from django.contrib.auth.models import User
-#from . import views
 
 class SpamMessageForm(forms.ModelForm):
-    spam_message = forms.CharField(max_length=140)
+    spam_messages = forms.CharField(max_length=140)
     
     class Meta:
         model = SpamMessage
@@ -15,19 +14,23 @@ class SpamMessageForm(forms.ModelForm):
 class LoginForm(AuthenticationForm):
     pass
 
-class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ('username', 'first_name', 'email', 'password', 'password2',)
 
-    def save(self, commit=True):
-        user = super(UserRegistrationForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not match.')
+
+        return cd['password2']
 
 
 
--- sirspamalot/userinfo/models.py
+++ sirspamalot/userinfo/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings #might need to delet
 from django.core.urlresolvers import reverse
 from datetime import datetime 
 from django.http import HttpResponse
@@ -7,12 +6,12 @@ from django.contrib.auth.models import User
  
    
 class SpamMessage(models.Model):
-    date_created = models.DateTimeField('date created', default=datetime.now)
+    date_posted = models.DateTimeField('date posted', default=datetime.now)
     user = models.ForeignKey(User)
     spam_message = models.CharField(max_length=140)
     
     class Meta:
-        ordering = ['date_created']
+        ordering = ['date_posted']
 
     def __str__(self):
         return self.spam_message
@@ -22,9 +21,14 @@ class SpamMessage(models.Model):
 class Profile(models.Model):
     # Relations
     user = models.OneToOneField(User)#(settings.AUTH_USER_MODEL, related_name="profile")
+    date = models.DateTimeField('date profile created', default=datetime.now)
+    user_bio = models.CharField(max_length=500, blank=True)
 
+    #def __str__(self):
+     #   return self.user.username
+ 
     def get_absolute_url(self):
-        return reverse('spam_message : user_page', kwargs={'id': self.pk})
+       return reverse('spam_message : user_page', kwargs={'id': self.pk})
     
    
      
